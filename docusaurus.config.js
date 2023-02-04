430b3c2e6d8d7d138f881b817ef1bb9bc37b5bb5
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2023-02-04 06:10:45 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,9 +68,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'RMP',
         logo: {
-          alt: 'My Site Logo',
+          alt: "RMP Logo',
           src: 'img/logo.svg',
         },
         items: [
